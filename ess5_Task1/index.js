7aa456ddcbc34e6868c887d01be0de461391d34c
Николай Давыденко <davydenko.kolya@bk.ru>
2021-03-15 12:06:37 +0300

    comlete first task

--- ess5_Task1/index.js
+++ ess5_Task1/index.js
@@ -5,29 +5,39 @@ Array.prototype.findMax = function () {
         if (+this[i] > max)
             max = +this[i];
     }
-    alert(max);
+    return max;
+}
+
+Array.prototype.findMin = function () {
+    let min = 100000000000;
+    for (let i = 0; i < this.length; i++) {
+        if (+this[i] < min)
+            min = +this[i];
+    }
+    return min;
+}
+
+Array.prototype.calculateAverage = function () {
+    let average = 0;
+    for (let i = 0; i < this.length; i++) {
+        average += +this[i];
+    }
+    return average / this.length;
 }
 
 
-function find() {
+function findMax() {
     let array = document.getElementById('array').value.split(' ');
-    
+
     document.getElementById('answer').value = array.findMax();
 }
 function findMin() {
     let array = document.getElementById('array').value.split(' ');
-    let min = 100000000000;
-    for (let i = 0; i < array.length; i++) {
-        if (+array[i] < min)
-            min = +array[i];
-    }
-    document.getElementById('answer').value = min;
+
+    document.getElementById('answer').value = array.findMin();
 }
 function calculateAverage() {
     let array = document.getElementById('array').value.split(' ');
-    let average = 0;
-    for (let i = 0; i < array.length; i++) {
-        average += +array[i];
-    }
-    document.getElementById('answer').value = average / array.length;
+
+    document.getElementById('answer').value = array.calculateAverage();
 }
