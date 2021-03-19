d5a73f6053287fb2ff869fb929bbb07fe17c7604
Николай Давыденко <davydenko.kolya@bk.ru>
2021-03-19 11:02:35 +0300

    add sorts

--- ess5_Task1/index.js
+++ ess5_Task1/index.js
@@ -41,3 +41,66 @@ function calculateAverage() {
 
     document.getElementById('answer').value = array.calculateAverage();
 }
+
+function bubbleSort() {
+    let array = document.getElementById('array').value.split(' ');
+
+    let f = 1;
+    while (f == '1') {
+        f = 0;
+        for (let i = 1; i < array.length; i++)
+            if (array[i - 1] > array[i]) {
+                let buff = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = buff;
+                f = 1;
+            }
+    }
+    document.getElementById('answer').value = array;
+}
+
+function bubbleSort() {
+    let array = document.getElementById('array').value.split(' ');
+
+    let f = 1;
+    while (f == '1') {
+        f = 0;
+        for (let i = 1; i < array.length; i++)
+            if (+array[i - 1] > +array[i]) {
+                let buff = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = buff;
+                f = 1;
+            }
+    }
+    document.getElementById('answer').value = array;
+}
+
+function selectSort() {
+    let array = document.getElementById('array').value.split(' ');
+
+    for (let i = 0; i < array.length - 1; i++) {
+        let min = +array[i];
+        let index;
+        for (let j = i + 1; j < array.length; j++) {
+            if (+array[j] < min) {
+                min = array[j];
+                index = j;
+            }
+        }
+        array[index] = array[i];
+        array[i] = min;
+    }
+    document.getElementById('answer').value = array;
+
+}
+
+function quickSort() {
+    let array = document.getElementById('array').value.split(' ');
+
+    array.sort(compareNumbers);
+    function compareNumbers(a, b) {
+        return a - b;
+      }
+    document.getElementById('answer').value = array;
+}
