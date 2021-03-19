70ca41128fdffa3b8d30cc51313c7e1774e83173
Николай Давыденко <davydenko.kolya@bk.ru>
2021-03-19 12:12:56 +0300

    add convert

--- ess5_Task1/index.js
+++ ess5_Task1/index.js
@@ -101,6 +101,50 @@ function quickSort() {
     array.sort(compareNumbers);
     function compareNumbers(a, b) {
         return a - b;
-      }
+    }
     document.getElementById('answer').value = array;
+}
+
+function convert() {
+    let array = document.getElementById('array2').value.split(' ');
+
+    switch (firstselect.value) {
+        case '2':
+            array = parseInt(+array, 2);
+            break;
+        case '8':
+            array = parseInt(+array, 8);
+            break;
+        case '10':
+            array = parseInt(+array, 10);
+            break;
+        case '16':
+            array = parseInt(+array, 16);
+            break;
+
+        default:
+            alert("Нет таких значений");
+    }
+
+    switch (secondselect.value) {
+        case '2':
+            array = array.toString(2);
+            break;
+        case '8':
+            array = array.toString(8);
+            break;
+        case '10':
+            array = array.toString(10);
+            break;
+        case '16':
+            array = array.toString(16);
+            break;
+
+        default:
+            alert("Нет таких значений");
+    }
+
+
+
+    document.getElementById('answer2').value = array;
 }
