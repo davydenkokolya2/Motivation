fd2a388a830eeb5b75ec2d76d0fe50743db684e3
Николай Давыденко <davydenko.kolya@bk.ru>
2021-03-21 21:47:26 +0300

    add calculator

--- ess5_Task1/index.js
+++ ess5_Task1/index.js
@@ -148,3 +148,26 @@ function convert() {
 
     document.getElementById('answer2').value = array;
 }
+
+function plus() {
+    document.getElementById('calculator').value += "+";
+}
+function minus() {
+    document.getElementById('calculator').value += "-";
+}
+function multiply() {
+    document.getElementById('calculator').value += "*";
+}
+function split() {
+    document.getElementById('calculator').value += "/";
+}
+function equally() {
+    try {
+        if (eval(document.getElementById('calculator').value) != 'Infinity') {
+            document.getElementById('cache').value += document.getElementById('calculator').value + '\n';
+            document.getElementById('calculator').value = eval(document.getElementById('calculator').value);
+        }
+    } catch {
+
+    }
+}
