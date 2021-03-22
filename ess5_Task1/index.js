850a6b40571b6e2be4926c59c7bdf6c5637483d4
Николай Давыденко <davydenko.kolya@bk.ru>
2021-03-22 20:05:22 +0300

    add date

--- ess5_Task1/index.js
+++ ess5_Task1/index.js
@@ -171,3 +171,22 @@ function equally() {
 
     }
 }
+function selectdate() {
+    let date = new Date();
+    
+    switch (formatselect.value) {
+        case '1':
+            document.getElementById('date').value = date.getDate() + "." + (+date.getMonth() + +1) + "." + date.getFullYear();
+            break;
+        case '2':
+            document.getElementById('date').value = date.getDate() + "." + date.toLocaleString('en', {month:'long'}) + "." + date.getFullYear();
+            break;
+        case '3':
+            document.getElementById('date').value = (+date.getMonth() + +1) + "." + date.getDate()   + "." + date.getFullYear();
+            break;
+    
+        default:
+            alert("Нет таких значений");
+    }
+    /
+}
