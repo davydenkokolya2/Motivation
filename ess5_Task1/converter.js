ba60ade2f9d3a732d1549904c80325f39a786bed
Николай Давыденко <davydenko.kolya@bk.ru>
2021-03-29 21:56:46 +0300

    fix convert

--- ess5_Task1/converter.js
+++ ess5_Task1/converter.js
@@ -1,43 +1,8 @@
 function convert() {
     let array = document.getElementById('array2').value.split(' ');
 
-    switch (firstselect.value) {
-        case '2':
-            array = parseInt(+array, 2);
-            break;
-        case '8':
-            array = parseInt(+array, 8);
-            break;
-        case '10':
-            array = parseInt(+array, 10);
-            break;
-        case '16':
-            array = parseInt(+array, 16);
-            break;
-
-        default:
-            alert("Нет таких значений");
-    }
-
-    switch (secondselect.value) {
-        case '2':
-            array = array.toString(2);
-            break;
-        case '8':
-            array = array.toString(8);
-            break;
-        case '10':
-            array = array.toString(10);
-            break;
-        case '16':
-            array = array.toString(16);
-            break;
-
-        default:
-            alert("Нет таких значений");
-    }
-
-
+    array = parseInt(+array, firstselect.value);
+    array = array.toString(secondselect.value);
 
     document.getElementById('answer2').value = array;
 }
