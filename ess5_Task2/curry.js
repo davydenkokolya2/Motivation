eed888d0a72518ea42eb658479bf7737189e9d76
Николай Давыденко <davydenko.kolya@bk.ru>
2021-04-03 12:15:18 +0300

    add curry

---
+++ ess5_Task2/curry.js
@@ -0,0 +1,21 @@
+function curry(fn) {
+    return function curried() {
+        const args = Array.prototype.slice.call(arguments)
+        const done = args.length >= fn.length
+        if (done) {
+            return fn.apply(this, args)
+        } else {
+            return function () {
+                const args2 = Array.prototype.slice.call(arguments)
+                return curried.apply(this, args.concat(args2))
+            }
+        }
+    }
+}
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+var add = curry(sum);
+alert(add(2)(3)(4));
